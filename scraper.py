56a75c39bd45c4efc3d2da03ee27c7584a408c33
alehe <alehe98@gmx.de>
2021-11-07 15:33:26 +0100

    initial commit

---
+++ scraper.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+from openpyxl import Workbook
+
+URL = "https://www.worldometers.info/coronavirus/"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+table = soup.find("table", id="main_table_countries_today")
+all_bodies = soup.findAll("tbody")
+main_body = all_bodies[0]
+main_rows = main_body.findAll("tr")
+
+continents = []
+total_numbers = []
+for index_x in range(0, 6):
+    continents.append(main_rows[index_x].findAll("td")[1].text.strip())
+    total_numbers.append(main_rows[index_x].findAll("td")[2].text.strip())
+
+wb = Workbook()
+ws = wb.active
+ws.title = "Corona cases"
+
+for x_index in range(1, len(continents)):
+    ws['A' + str(x_index)] = continents[x_index]
+    ws['B' + str(x_index)] = total_numbers[x_index]
+
+wb.save('cases.xlsx')
